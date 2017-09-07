ecae497a42d578d84ef39a662c17bbb6494a9433
UtileHomme <jatins368@gmail.com>
2017-09-07 12:13:06 +0530

    Adobe photoshop files added

---
+++ All_Adobe_Photoshop_concepts.php
@@ -0,0 +1,19 @@
+<!-- What is the use of spot healing Brush tool -->
+
+Use the Spot Healing Brush Tool for Removing Pimples
+
+<!-- What is the use of "Liquify" option -->
+Use the Liquify option to change the contours of a person's face like make them smile etc.
+
+<!-- What is a Layer in Adobe Photoshop -->
+A Layer can be taken as a plastic cover which is put on a photo and a person starts writing on it. It wouldn't affect the photo underneath
+- We can turn Layears of and on
+
+<!-- What is the Brush tool -->
+Brush tool is same as a paint brush tool
+
+<!-- What is the Adjustment Tool -->
+There are adjustment layers which are available
+- One of them is the "exposure layer" or the layer mask
+- The exposure layer by default changes everything or nothing
+-
---
+++ All_Shortcuts.php
@@ -0,0 +1,28 @@
+<!-- Access the Spot Healing Brush tool -->
+
+Use "J"
+
+<!-- To Zoom in or Zoom out and image -->
+
+Use "Alt + Wheel" OR "Ctrl + "+/-"
+
+<!-- To access Liquify Option -->
+Use "Shift + Ctrl + F"
+
+<!-- To select "Brush" tool -->
+Use "B"
+
+<!-- To select Eraser Tool -->
+Use "E"
+
+<!-- To invert the image -->
+Use "Ctrl + I"
+
+<!-- To close all images -->
+Use "Alt + Ctrl + W"
+
+<!-- To close a particular image  -->
+Use "Ctrl + W"
+
+<!-- To step backward to previous operations -->
+use "Alt + Ctrl + Z"
